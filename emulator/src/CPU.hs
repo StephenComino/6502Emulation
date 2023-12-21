4851e8c9fe37ec65882757854e8d07d3da2087c2
Stephen Comino <Stephen.comino@aristocrat.com>
2023-12-21 17:09:11 +1100

    Added State Monad

--- emulator/src/CPU.hs
+++ emulator/src/CPU.hs
@@ -1,13 +1,47 @@
+{-# LANGUAGE TemplateHaskell, Rank2Types, NoMonomorphismRestriction #-}
 module CPU
     ( someFunc
     ) where
+import Data.Int
+import Control.Monad.State (State, execState, get)
+import Control.Lens
 
+data Processor = Processor { 
+  _ir :: Int16, 
+  _tcu :: Int, -- Timing Control Unit
+  _alu :: Int, -- Arithmetic and Logic Unit
+  _acc :: Int, -- Accumulator
+  _idx :: Int, -- Index Register X
+  _idy :: Int, -- Index Register Y
+  _pr :: Int, -- Processor Status Register
+  _pc :: Int, -- Process Counter
+  _sp :: Int -- Stack Pointer
+} deriving (Show)
+
+$(makeLenses ''Processor)
+
+type MyProcessor = State Processor Processor
+instance Show (State Processor Processor) where
+  show a   = 
+
+
+initial :: Processor
+initial = Processor 0 0 0 0 0 0 0 0 0
+
+setup :: State Processor Processor
+setup = do
+    p <- get
+    --p <- get
+    --return p
+    sp .= 1
+    return p
 -- registers
 -- IR register -- Instruction is loaded into this register from the data bus and latched during the 
 -- OpCode Fetch Cycle
 -- OpCode is then decoded, along with timing and interrup signals, to generate various control signals
-someFunc :: IO ()
+someFunc :: MyProcessor
 someFunc = undefined
+
 
 -- capable of addressing 65,536 bytes of memory
 
@@ -15,10 +49,7 @@ data OpCodes = ADC | AND | ASL | BBR | BBS | BBC | BCS | BEQ | BIT | BMI | BME |
                BRK | BVC | BVS | CLC | CLD | CLI | CLV | CMP | CPX | CPY | DEC | DEX | DEY | EOR |
                INC | INX | INY | JMP | JSR | LDA | LDX | LDY | LSR | NOP | ORA | PHA | PHP | PHX |
                PHY | PLA | PLP | PLX | PLY | RMB | ROL | ROR | RTI | RTS | SBC | SEC | SED | SEI |
-               SMB | STA | STA | STP | STX | STY | STZ | TAX | TAY | TRB | TSB | TSX | TXS | TYA |
-               WAI
+               SMB | STA | STP | STX | STY | STZ | TAX | TAY | TRB | TSB | TSX | TXS | TYA | WAI
 
 
 -- 1. LDA STA
-
-Registers 
