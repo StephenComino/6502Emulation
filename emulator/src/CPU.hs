82e873db10102e27e0640a647475db9912528343
Stephen <stephen@Stephens-MBP.lan>
2023-12-21 21:09:27 +1100

    Added Parser, Added to State, Tokens are OpCodes

--- emulator/src/CPU.hs
+++ emulator/src/CPU.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE TemplateHaskell, Rank2Types, NoMonomorphismRestriction #-}
 module CPU
-    ( someFunc
+    ( readInstructions
     ) where
+
 import Data.Int
-import Control.Monad.State (State, execState, get)
+import Control.Monad.State (State, execState, get, evalState)
+import Parser (parseInstruction, Command(..), Parser, parse, loadCommands, OpCodes(..))
+import Control.Monad.State.Lazy
 import Control.Lens
 
 data Processor = Processor { 
@@ -20,36 +23,31 @@ data Processor = Processor {
 
 $(makeLenses ''Processor)
 
-type MyProcessor = State Processor Processor
-instance Show (State Processor Processor) where
-  show a   = 
-
 
 initial :: Processor
 initial = Processor 0 0 0 0 0 0 0 0 0
 
+-- execState setup initial
+-- Increment the State... Stack pointer by one
 setup :: State Processor Processor
 setup = do
     p <- get
-    --p <- get
-    --return p
-    sp .= 1
+    res <- use sp
+    sp .= res + 1
     return p
--- registers
--- IR register -- Instruction is loaded into this register from the data bus and latched during the 
--- OpCode Fetch Cycle
--- OpCode is then decoded, along with timing and interrup signals, to generate various control signals
-someFunc :: MyProcessor
-someFunc = undefined
 
+-- Implement only # addressing first
+-- LDX A2
+readInstructions :: State Processor Processor
+readInstructions = do
+              p <- get
+              let m = getParsedValue $ parse parseInstruction loadCommands
+              res <- use sp
+              sp .= address m 
+              return p
 
--- capable of addressing 65,536 bytes of memory
-
-data OpCodes = ADC | AND | ASL | BBR | BBS | BBC | BCS | BEQ | BIT | BMI | BME | BNE | BPL | BRA |
-               BRK | BVC | BVS | CLC | CLD | CLI | CLV | CMP | CPX | CPY | DEC | DEX | DEY | EOR |
-               INC | INX | INY | JMP | JSR | LDA | LDX | LDY | LSR | NOP | ORA | PHA | PHP | PHX |
-               PHY | PLA | PLP | PLX | PLY | RMB | ROL | ROR | RTI | RTS | SBC | SEC | SED | SEI |
-               SMB | STA | STP | STX | STY | STZ | TAX | TAY | TRB | TSB | TSX | TXS | TYA | WAI
-
-
+getParsedValue :: Either a (Command, [Char]) -> Command
+getParsedValue a = case a of
+                      Left err -> Command { address=0}
+                      Right (com, rest) -> com
 -- 1. LDA STA
---
+++ emulator/src/Parser.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE TemplateHaskell, FlexibleInstances, LambdaCase, OverloadedStrings #-}
+module Parser (parseInstruction, Command(..), Parser, parse, loadCommands, OpCodes) where
+
+import Control.Applicative
+import Data.Char
+import System.IO.Unsafe
+import Control.Monad.State.Lazy
+import Control.Lens
+
+data Error = Error deriving (Show)
+
+
+data OpCodes = ADC | AND | ASL | BBR | BBS | BBC | BCS | BEQ | BIT | BMI | BME | BNE | BPL | BRA |
+               BRK | BVC | BVS | CLC | CLD | CLI | CLV | CMP | CPX | CPY | DEC | DEX | DEY | EOR |
+               INC | INX | INY | JMP | JSR | LDA | LDX | LDY | LSR | NOP | ORA | PHA | PHP | PHX |
+               PHY | PLA | PLP | PLX | PLY | RMB | ROL | ROR | RTI | RTS | SBC | SEC | SED | SEI |
+               SMB | STA | STP | STX | STY | STZ | TAX | TAY | TRB | TSB | TSX | TXS | TYA | WAI deriving (Show)
+
+data Command = Command { instruction :: OpCodes, address :: Int} deriving (Show)
+
+$(makeLenses ''Command)
+
+newtype Parser a i = Parser {
+    parse :: [a] -> Either Error (i, [a])
+}
+
+$(makeLenses ''Parser)
+
+instance Functor (Parser a) where
+    fmap f (Parser a) = Parser $ \input -> 
+        case a input of
+            Left err -> Left err
+            Right (x, rest) -> Right (f x, rest)
+
+instance Applicative (Parser a) where
+    pure a = Parser $ \input -> Right (a, input)
+    Parser a <*> Parser b = Parser $ \input ->
+        case a input of
+            Left err -> Left err
+            Right (x, rest) -> 
+                case b rest of
+                    Left err -> Left err
+                    Right (y, rest') -> Right (x y, rest')
+
+instance Alternative (Parser a) where
+    empty = Parser $ \input -> Left Error
+    Parser a <|> Parser b = Parser $ \input ->
+        case a input of
+            Left err -> 
+                case b input of
+                    Left err -> Left Error
+                    Right (x,xs) -> Right (x,xs)
+            Right (x,xs) -> Right (x,xs)
+
+instance Monad (Parser i) where
+  return = pure
+  Parser p >>= k = Parser $ \input ->
+    case p input of
+      Left err -> Left err
+      Right (output, rest) ->
+        let
+          Parser p' = k output
+        in
+        p' rest
+
+satisfy :: (s -> Bool) -> Parser s s
+satisfy f = Parser $ \case
+        [] -> Left Error
+        x:rest
+            | f x -> Right (x, rest)
+            | otherwise -> Left Error
+
+-- *> -- (*>) readChar 'l' readChar 'l'
+readChar :: (Eq s) => s -> Parser s s 
+readChar x = Parser f 
+    where
+        f (y:ys)
+            | y == x    = Right (x,ys)
+            | otherwise = Left Error
+        f [] = Left Error
+-- | Parser for null json
+
+-- How does this work?
+readString :: [Char] -> Parser Char [Char]
+readString str = Parser $ \input ->
+    case parse (traverse readChar str) input of
+        Left err -> Left err
+        Right (x, rest) -> Right (x, rest)
+
+-- Define LAnguage Things
+parseInstruction :: Parser Char Command
+parseInstruction = do
+    instruction <- loadX
+    _ <- parseSpace
+    address <- many $ getAddress
+    return $ Command { instruction=instruction, address= hexToDecimal address }
+
+hexToDecimal :: String -> Int
+hexToDecimal = sum . zipWith (*) (iterate (*16) 1) . reverse . map digitToInt . map toUpper
+
+getAddress :: Parser Char Char
+getAddress = satisfy isAlpha <|> satisfy isDigit
+
+-- Program Start
+parseSpace :: Parser Char [Char]
+parseSpace = readString " "
+
+loadX :: Parser Char OpCodes
+loadX = LDX <$ readString "A2"
+
+loadCommands :: [Char]
+loadCommands = do
+    contents <- unsafePerformIO $ readFile "/Users/stephen/6502Emulation/emulator/src/examples/instructions.bin"
+    return $ contents
+    --startToken putStrLn "someFunc"
